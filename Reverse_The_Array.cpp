b59715c57749762597e337a45635d770443be90d
Deepesh-Sharma-4628 <104193104+Deepesh-Sharma-4628@users.noreply.github.com>
2022-12-29 22:12:29 +0530

    Initial Commit

---
+++ Reverse_The_Array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+            int start=m+1;
+            int end=arr.size()-1;
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
